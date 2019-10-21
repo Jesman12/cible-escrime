89ef76de1999e5b1baaab912a97c009dcb13c817
Jesus manuel CUERVO ITURBIDE <jesus-manuel.cuervo-iturbide@etu.univ-nantes.fr>
2019-10-21 14:00:56 +0200

    IP -> NOMBRE(id)

--- index.php
+++ index.php
@@ -231,7 +231,8 @@
 									var pos_IP_FIN = IP_cible.indexOf(buscar_IP_FIN);
 									var pos_ID_FIN = ID_cible.indexOf(" IP:");
 									IP_cible = IP_cible.substr(0,pos_IP_FIN-2);
-									ID_cible = ID_cible.substr(0,pos_ID_FIN-1);
+									alert(ID_cible);
+									ID_cible = ID_cible.substr(0,pos_ID_FIN);
 									var btn_conectar = document.createElement("button");
 									var P_IP = document.createElement("p");
 									var A_IP = document.createElement("a");
