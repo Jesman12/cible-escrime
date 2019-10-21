3ba901bb763099b5c2014c178cdad0ddd830498b
Jesus manuel CUERVO ITURBIDE <jesus-manuel.cuervo-iturbide@etu.univ-nantes.fr>
2019-10-21 11:51:55 +0200

    Filtro IP

--- index.php
+++ index.php
@@ -224,8 +224,7 @@
 								var buscar_IP_FIN = "</html>";
 								var pos_IP_cible = data.indexOf(buscar_IP_cible);
 								var IP_cible = data.substr(pos_IP_cible+4);
-								alert(IP_cible.length);
-							if (IP_cible.length <= 15) {
+							if (IP_cible.length <= 30) {
 									var pos_IP_FIN = IP_cible.indexOf(buscar_IP_FIN);
 									IP_cible = IP_cible.substr(0,pos_IP_FIN-2);
 									var btn_conectar = document.createElement("button");
