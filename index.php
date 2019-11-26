1bfe0743ab05e54067636c219e26a661095b76f0
Jesman <jesman2012@gmail.com>
2019-11-26 07:50:25 -0600

    TEST

--- index.php
+++ index.php
@@ -530,9 +530,10 @@
 				}
 				rep = false;
 			}
+			var elementosCibles = document.getElementsByClassName('Nombre');
 			for(var i = 0; i < Cibles_Score.length; i++){
 				setTimeout(function(){
-						var id_Nom = document.getElementsByClassName('Nombre')[i].id;
+						var id_Nom = elementosCibles[i].id;
 						var Nombre_id = id_Nom.substr(3);
 						tipo = Nombre_id;
 						document.getElementById('Puntaje'+i).innerHTML = Cibles_Score[i].innerHTML;
